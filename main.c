c3d73b176bfd5dd4806bad0c2acceff4b8c5c3fe
subwafer <subwafer@proton.me>
2023-03-15 19:55:13 -0400

    implemented a create_todo function

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 bool DEBUG_MODE = false;
 
+int create_todo(char *file_path, char *todo_content);
 char *read_from_file(char *file_path, size_t *content_size);
 
 int main(int argc, char **argv) {
@@ -29,10 +30,36 @@ int main(int argc, char **argv) {
 
     size_t content_size = 0;
     char *todos = read_from_file(file_path, &content_size);
-
     fwrite(todos, 1, content_size, stdout);
 
+    // create_todo
+    char *test_todo = "test todo\n";
+    create_todo(file_path, test_todo);
+
+    todos = read_from_file(file_path, &content_size);
+
+    printf("--------------------\n");
+    fwrite(todos, 1, content_size, stdout);
+
+    // delete_todo
+    // edit_todo
     return 0;
+}
+
+int create_todo(char *file_path, char *todo_content) {
+    // get the file, move cursor to end, write todo into it
+    FILE *file = fopen(file_path, "a");
+    if (file == NULL) {
+        fprintf(stderr, "ERROR: Couldn't open file (w): %s\n", file_path);
+        goto error;
+    }
+
+    fwrite(todo_content, 1, strlen(todo_content), file);
+
+error:
+    if (file) fclose(file);
+
+    return 1;
 }
 
 char *read_from_file(char *file_path, size_t *content_size) {
